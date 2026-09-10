c3c91db2730acf5e3376f9a3362643da0283dd9d
eshahabian <eshahabian@gmail.com>
2026-09-10 18:37:37 +0330

    update 238

--- cpanel/includes/layout.php
+++ cpanel/includes/layout.php
@@ -51,7 +51,7 @@ if ($user && ($user['role'] ?? '') === 'SECRETARY') {
   <?= seo_render_head() ?>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@400;600;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="<?= e(url('/assets/css/style.css')) ?>?v=20260910x">
+  <link rel="stylesheet" href="<?= e(url('/assets/css/style.css')) ?>?v=20260910y">
   <meta name="csrf-token" content="<?= e(csrf_token()) ?>">
   <script>
   (function(){
